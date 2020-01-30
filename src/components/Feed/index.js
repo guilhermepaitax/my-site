7a2ecbb63e11b0729313a94f646119cfc09b819d
guilhermepaitax <guilhermepaitax16@gmail.com>
2020-01-30 16:10:02 -0300

    adding scroll for body

--- src/components/Feed/index.js
+++ src/components/Feed/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import { Container } from './styles';
 import About from './About';
@@ -7,8 +7,21 @@ import Experiences from './Experiences';
 import Projects from './Projects';
 
 export default function Feed() {
+  const feedRef = useRef(null);
+
+  useEffect(() => {
+    function handleScroll(e) {
+      if (feedRef.current && e.srcElement !== feedRef.current) {
+        feedRef.current.scrollTop += e.deltaY;
+      }
+    }
+
+    document.body.addEventListener('mousewheel', handleScroll);
+
+    return () => document.body.removeEventListener('mousewheel', handleScroll);
+  }, []);
   return (
-    <Container>
+    <Container ref={feedRef}>
       <About />
       <Knowledge />
       <Experiences />
