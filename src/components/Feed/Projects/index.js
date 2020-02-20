3eb1f7a6fc29d2b7e045b841cd6fc3351f92ce5e
guilhermepaitax <guilhermepaitax16@gmail.com>
2020-02-20 16:05:21 -0300

    projects section

--- src/components/Feed/Projects/index.js
+++ src/components/Feed/Projects/index.js
@@ -1,20 +1,60 @@
-import React, { useContext, useRef, useEffect } from 'react';
+import React, { useContext, useRef, useEffect, useState } from 'react';
 
-import { Container } from './styles';
+import {
+  FaChevronLeft,
+  FaChevronRight,
+  FaExternalLinkAlt,
+} from 'react-icons/fa';
+import { Container, Carousel, Project, Button } from './styles';
 import { Title, Index } from '../../GeneralComponents';
 import ThemeContext from '../../ThemeContext';
 import SectionContext from '../../SectionContext';
+
+import ImageProject2 from '~/assets/computer2.jpg';
+import ImageProject from '~/assets/computer.jpg';
+import ImageProject3 from '~/assets/computer.png';
+
+const projects = [
+  {
+    background: ImageProject2,
+    title: 'Music Player UI',
+    url: 'https://github.com/guilhermepaitax/juno-landing-page',
+  },
+  {
+    background: ImageProject,
+    title: 'App Bank Bitcoin',
+    url: 'https://github.com/guilhermepaitax/juno-landing-page',
+  },
+  {
+    background: ImageProject3,
+    title: 'Dark Web App',
+    url: 'https://github.com/guilhermepaitax/juno-landing-page',
+  },
+];
 
 export default function Projects() {
   const { themeActive } = useContext(ThemeContext);
   const { handleAddSections } = useContext(SectionContext);
   const sectionRef = useRef(null);
+  const [currentProject, setCurrentProject] = useState(0);
 
   useEffect(() => {
     if (sectionRef.current) {
       handleAddSections('projects', sectionRef.current);
     }
   }, [handleAddSections]);
+
+  function handleSlideProject(action) {
+    if (action === 'next') {
+      projects.length - 1 === currentProject
+        ? setCurrentProject(0)
+        : setCurrentProject(currentProject + 1);
+    } else if (action === 'prev') {
+      currentProject === 0
+        ? setCurrentProject(projects.length - 1)
+        : setCurrentProject(currentProject - 1);
+    }
+  }
 
   return (
     <Container ref={sectionRef} name="projects">
@@ -23,6 +63,30 @@ export default function Projects() {
         <Index themeActive={themeActive}>1</Index>
         <p>Meus Projetos.</p>
       </Title>
+      <Carousel>
+        <Project background={projects[currentProject].background}>
+          <Button
+            type="button"
+            onClick={() => handleSlideProject('next')}
+            side="next"
+          >
+            <FaChevronRight color="#fff" />
+          </Button>
+          <Button
+            type="button"
+            onClick={() => handleSlideProject('prev')}
+            side="prev"
+          >
+            <FaChevronLeft color="#fff" />
+          </Button>
+          <div>
+            <span>{projects[currentProject].title}</span>
+            <a href={projects[currentProject].url} title="Acessar Projeto">
+              <FaExternalLinkAlt size={18} color="#fff" />
+            </a>
+          </div>
+        </Project>
+      </Carousel>
     </Container>
   );
 }
--- src/components/Feed/Projects/styles.js
+++ src/components/Feed/Projects/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { fade } from '~/styles/animations';
 
@@ -19,5 +19,89 @@ export const Container = styled.div`
     animation: ${fade} 0.6s ease;
     color: #a6a8b9;
     text-align: justify;
+  }
+`;
+
+export const Carousel = styled.div`
+  margin-top: 60px;
+  width: 100%;
+  max-height: 400px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Project = styled.div`
+  border-radius: 8px;
+  background-image: url(${props => props.background});
+  background-position: center;
+  background-size: 100%;
+  background-repeat: no-repeat;
+  width: 100%;
+  height: 240px;
+  overflow: hidden;
+  position: relative;
+  transition: all 0.6s ease;
+
+  &:hover {
+    background-size: 110%;
+
+    button {
+      opacity: 1;
+    }
+  }
+
+  > div {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 50px;
+    backdrop-filter: blur(20px);
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 20px;
+    opacity: 0;
+    transition: all 0.5s ease-out;
+
+    span {
+      color: #fff;
+    }
+
+    &:hover {
+      opacity: 1;
+    }
+  }
+`;
+
+export const Button = styled.button`
+  position: absolute;
+  bottom: 50%;
+  transform: translateY(50%);
+  border: 0;
+  background: none;
+  border-radius: 12px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+  backdrop-filter: blur(20px);
+  opacity: 0;
+  transition: opacity 0.5s ease, transform 0.1s ease;
+
+  ${props => {
+    if (props.side === 'next') {
+      return css`
+        right: 15px;
+      `;
+    }
+    return css`
+      left: 15px;
+    `;
+  }}
+
+  &:hover {
+    transform: translateY(50%) scale(1.1);
   }
 `;
